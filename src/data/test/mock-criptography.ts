e75741bf9ab284e54c54ddf1f2b49f0c1b0de9fa
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2022-02-26 16:00:07 -0400

    refactor: change Promises to use shorter version

--- src/data/test/mock-criptography.ts
+++ src/data/test/mock-criptography.ts
@@ -6,7 +6,7 @@ import { HashComparer } from '@/data/protocols/criptography/hash-comparer'
 export const mockHasher = (): Hasher => {
   class HasherStub implements Hasher {
     async hash (value: string): Promise<string> {
-      return await new Promise(resolve => resolve('hashed_password'))
+      return await Promise.resolve('hashed_password')
     }
   }
 
@@ -16,7 +16,7 @@ export const mockHasher = (): Hasher => {
 export const mockDecrypter = (): Decrypter => {
   class DecrypterStub implements Decrypter {
     async decrypt (value: string): Promise<string> {
-      return await new Promise(resolve => resolve('any_value'))
+      return await Promise.resolve('any_value')
     }
   }
   return new DecrypterStub()
@@ -25,7 +25,7 @@ export const mockDecrypter = (): Decrypter => {
 export const mockEncrypter = (): Encrypter => {
   class EncrypterStub implements Encrypter {
     async encrypt (value: string): Promise<string> {
-      return await new Promise(resolve => resolve('any_token'))
+      return await Promise.resolve('any_token')
     }
   }
   return new EncrypterStub()
@@ -34,7 +34,7 @@ export const mockEncrypter = (): Encrypter => {
 export const mockHashComparer = (): HashComparer => {
   class HashComparerStub implements HashComparer {
     async compare (value: string, hash: string): Promise<boolean> {
-      return await new Promise(resolve => resolve(true))
+      return await Promise.resolve(true)
     }
   }
   return new HashComparerStub()
