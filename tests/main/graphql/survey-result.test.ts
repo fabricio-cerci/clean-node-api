66dd466862534e7cfaee7dc0c7bbf006018fa3e7
Fabrício Cerci <fabriciocerci@gmail.com>
2022-03-07 00:25:13 -0400

    test: ensure SaveSurveyResult Query returns SurveyResult on success

--- tests/main/graphql/survey-result.test.ts
+++ tests/main/graphql/survey-result.test.ts
@@ -47,7 +47,7 @@ describe('SurveyResult GraphQL ', () => {
     await accountCollection.deleteMany({})
   })
 
-  describe('Surveys Query', () => {
+  describe('SurveyResult Query', () => {
     const surveyResultQuery = gql`
       query surveyResult ($surveyId: String!) {
         surveyResult (surveyId: $surveyId){
@@ -124,6 +124,66 @@ describe('SurveyResult GraphQL ', () => {
       })
       expect(res.data).toBeFalsy()
       expect(res.errors[0].message).toBe('Access denied')
+    })
+  })
+
+  describe('SavSurveyResult Mutation', () => {
+    const saveSurveyResultMutation = gql`
+      mutation saveSurveyResult ($surveyId: String!, $answer: String!) {
+        saveSurveyResult (surveyId: $surveyId, answer: $answer){
+          question
+          answers {
+            answer
+            count
+            percent
+            isCurrentAccountAnswer
+          }
+          date
+        }
+      }
+    `
+
+    test('Should return SurveyResult', async () => {
+      const accessToken = await mockAccessToken()
+      const now = new Date()
+      const surveyRes = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: now
+      })
+      const { mutate } = createTestClient({
+        apolloServer,
+        extendMockRequest: {
+          headers: {
+            'x-access-token': accessToken
+          }
+        }
+      })
+      const res: any = await mutate(saveSurveyResultMutation, {
+        variables: {
+          surveyId: surveyRes.ops[0]._id.toString(),
+          answer: 'Answer 1'
+        }
+      })
+      expect(res.data.saveSurveyResult.question).toBe('Question')
+      expect(res.data.saveSurveyResult.date).toBe(now.toISOString())
+      expect(res.data.saveSurveyResult.answers).toEqual([{
+        answer: 'Answer 1',
+        count: 1,
+        percent: 100,
+        isCurrentAccountAnswer: true
+      }, {
+        answer: 'Answer 2',
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }
+      ])
     })
   })
 })
